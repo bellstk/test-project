eb45a7ecaa6b795c47c8d730e9b3a76304ff0832
Bellst <yaroslav.estwhite@gmail.com>
2024-08-06 15:14:09 +0500

    Автогенерация API. Начальная точка проекта.

---
+++ AuthServ/src/main/java/org/openapitools/api/UsersApiController.java
@@ -0,0 +1,25 @@
+package org.openapitools.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-08-06T14:49:25.152364100+05:00[Asia/Yekaterinburg]")
+
+@Controller
+@RequestMapping("${openapi.authenticationService.base-path:}")
+public class UsersApiController implements UsersApi {
+
+    private final NativeWebRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public UsersApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
---
+++ AuthServ/src/test/java/ru/bellst/AuthServ/AuthServApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bellst.AuthServ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
